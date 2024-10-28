3f5587ef581526a6d68629f8bba7a2071259e7e1
dantehurr <dantehurr@gmail.com>
2024-10-28 17:31:21 -0400

    Updates to CSS

--- setter-social/src/Welcome.js
+++ setter-social/src/Welcome.js
@@ -1,14 +1,18 @@
+import './theme.css';
 import './App.css';
 
 function Welcome() {
     return(
         <div className='flex-center-col'>
-            <div>
+            <div className='text-center'>
+                <h1>Welcome to SetterSocial!</h1>
+                <h2>Welcome to SetterSocial!</h2>
                 <h3>Welcome to SetterSocial!</h3>
+                <h4>Welcome to SetterSocial!</h4>
             </div>
             <div className='buttons-group'>
-                <button className='button-primary'>Create Account</button>
-                <button  className='button-primary'>Log In</button>
+                <button className='button__primary'>Create Account</button>
+                <button  className='button__primary'>Log In</button>
             </div>
         </div>
     );
