d40aa77d69e3d18738aee9d331649dab51a93767
dantehurr <dantehurr@gmail.com>
2024-11-03 14:04:18 -0500

    Completed Form Component Styling

--- setter-social/src/components/forms/AddSocialMediaForm.js
+++ setter-social/src/components/forms/AddSocialMediaForm.js
@@ -2,17 +2,21 @@ import '../../styles/forms.css';
 
 const AddSocial = () => {
     return(
-        <div>
+        <div className='form__wrapper'>
             <h2>Add Social Media Accounts</h2>
-            <form>
-                <label for="insta">Instagram</label>
-                <input id="addInstagram" type="text" name="insta"></input>
-
-                <label for="discord">Discord</label>
-                <input id="addDiscord" type="text" name="discord"></input>
-
-                <label for="twitter">Twitter/X</label>
-                <input id="addTwitter" type="text" name="twitter"></input>
+            <form className='form__container-flex'>
+                <div className='form__field'>
+                    <label for="insta">Instagram</label>
+                    <input id="addInstagram" type="text" name="insta"></input>
+                </div>
+                <div className='form__field'>
+                   <label for="discord">Discord</label>
+                    <input id="addDiscord" type="text" name="discord"></input> 
+                </div>
+                <div className='form__field'>
+                   <label for="twitter">Twitter/X</label>
+                    <input id="addTwitter" type="text" name="twitter"></input> 
+                </div>        
             </form>
         </div>
     );
--- setter-social/src/components/forms/BasicProfileForm.js
+++ setter-social/src/components/forms/BasicProfileForm.js
@@ -2,20 +2,33 @@ import '../../styles/forms.css';
 
 const BasicProfileForm = () => {
     return(
-        <div className='basic__profile'>
+        <div className='form__wrapper'>
             <h2>Basic Profile</h2>
-            <form className='basic__profile__form'>
-                <label for="name">Display Name</label>
-                <input id="addDisplayName" type="text" name="name"></input>
-                
-                <label for="major">Major</label>
-                <input id="addMajor" type="text" name="major" className='major__input'></input>
-
-                <label for="class">Class Year</label>
-                <input id="addClass" type="text" name="class" className='class__input'></input>
-
-                <label for="bio">Bio</label>
-                <input id="addBio" type="text" name="bio"></input>
+            <form className='form__container-grid'>
+                <div style={{ gridColumn: 'span 3' }}>
+                    <div className='form__field'>
+                        <label for="name">Display Name</label>
+                        <input id="addDisplayName" type="text" name="name"></input>
+                    </div>
+                </div>
+                <div style={{ gridColumn: 'span 2' }}>
+                <div className='form__field'>
+                    <label for="major">Major</label>
+                    <input id="addMajor" type="text" name="major" className='major__input'></input>
+                </div>
+                </div>
+                <div style={{ gridColumn: 'span 1' }}>
+                <div className='form__field'>
+                    <label for="class">Class Year</label>
+                    <input id="addClass" type="text" name="class" className='class__input'></input>
+                </div>
+                </div>
+                <div style={{ gridColumn: 'span 3' }}>
+                <div className='form__field'>
+                    <label for="bio">Bio</label>
+                    <input id="addBio" type="text" name="bio"></input>
+                </div>
+                </div>
             </form>
         </div>
     );
