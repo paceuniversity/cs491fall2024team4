6a724f0d416dd8599da8803fc6cecbb0f293e8b0
dantehurr <dantehurr@gmail.com>
2024-11-10 19:28:47 -0500

    Improved Footer Menu

--- setter-social/src/App.js
+++ setter-social/src/App.js
@@ -9,16 +9,21 @@ import Signup from './pages/Signup';
 import CreateProfile from './pages/CreateProfile';
 import ProfileEdit from './pages/ProfileEdit';
 import CreateEvent from './pages/CreateEvent';
+import { ConfigProvider } from 'antd';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Welcome />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path='/signup/newprofile' element={<CreateProfile />} />
-      <Route path='/profile/edit' element={<ProfileEdit />} />
-      <Route path='/events/newevent' element={<CreateEvent />} />
-    </Routes>
+    <>
+      <ConfigProvider theme={{}}></ConfigProvider>
+      <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path='/signup/newprofile' element={<CreateProfile />} />
+          <Route path='/profile/edit' element={<ProfileEdit />} />
+          <Route path='/events/newevent' element={<CreateEvent />} />
+      </Routes>
+    </>
+
   );
 }
 
--- setter-social/src/components/footers/FooterMenu.js
+++ setter-social/src/components/footers/FooterMenu.js
@@ -2,20 +2,50 @@ import '../../styles/text.css';
 import '../../styles/buttons.css';
 import '../../styles/footer.css';
 
+import { useState } from 'react';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faUsers, faHouse, faCalendar, faUser } from '@fortawesome/free-solid-svg-icons';
 
 import { Menu } from 'antd';
 
+const menuItems = [
+    {
+        label: 'GO! Chat',
+        key: 'chat',
+        icon: <FontAwesomeIcon icon={faComment} />,
+    },
+    {
+        label: 'Groups',
+        key: 'groups',
+        icon: <FontAwesomeIcon icon={faUsers} />,
+    },
+    {
+        label: 'Feed',
+        key: 'feed',
+        icon: <FontAwesomeIcon icon={faHouse} />,
+    },
+    {
+        label: 'Events',
+        key: 'events',
+        icon: <FontAwesomeIcon icon={faCalendar} />,
+    },
+    {
+        label: 'Profile',
+        key: 'profile',
+        icon: <FontAwesomeIcon icon={faUser} />,
+    },
+];
+
 const FooterMenu = () => {
+    const [ current, setCurrent ] = useState('feed');
+    const menuNav = (e) => {
+        console.log("menu clicked: ", e)
+        setCurrent(e.key);
+    };
     return(
         <div className='footer__menu-container'>
-            <Menu mode='horizontal'>
-                <Menu.Item key='chat' icon={<FontAwesomeIcon icon={faComment} />}>Chat</Menu.Item>
-                <Menu.Item key='groups' icon={<FontAwesomeIcon icon={faUsers} />}>Groups</Menu.Item>
-                <Menu.Item key='feed' icon={<FontAwesomeIcon icon={faHouse} />}>Feed</Menu.Item>
-                <Menu.Item key='events' icon={<FontAwesomeIcon icon={faCalendar} />}>Events</Menu.Item>
-                <Menu.Item key='profile' icon={<FontAwesomeIcon icon={faUser} />}>Profile</Menu.Item>
+            <Menu mode='horizontal' onClick={menuNav} selectedKeys={[current]} items={menuItems}>
             </Menu>
         </div>
     );
